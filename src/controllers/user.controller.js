db93310a8acfbcec83416898f85209cd2203f9d4
git-pixel22 <workemail.rajat@gmail.com>
2024-04-26 19:04:32 +0530

    minor updates, adds some instructions on registering user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,27 @@
 import asyncHandler from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "User is registered brah!!"
-    })
+
+    // get user details from frontend
+    const {fullName, email, username, password } = req.body;
+
+    // validation - not empty
+    if(fullName === "" && email === "" && username === "" && password === "") {
+        throw new ApiError("400", "All fields are required")
+    }
+
+
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, check avatar again
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // returen response
+
+    // console.log("email: ", email);
+    // console.log("password: ", password);
 })
 
 export {registerUser}
