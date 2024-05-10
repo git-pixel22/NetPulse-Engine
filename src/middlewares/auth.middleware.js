539fee7fd44e67b2583079ad5c791d78d1f3ed73
git-pixel22 <workemail.rajat@gmail.com>
2024-05-11 03:53:49 +0530

    adds helpful and better comments to auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
-
+// Verifies the legitimacy of a user via the access token provided in the request and attaches user data to the request object.
 export const verifyJWT = asyncHandler( async(req, _res, next) => {
 
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "");
@@ -12,16 +12,19 @@ export const verifyJWT = asyncHandler( async(req, _res, next) => {
         throw new ApiError(401, "Unauthorized Request");
     }
 
-    // returns a decoded payload that we passed to jwt.sign() while creating accessToken. If the token is invalid a error is thrown by .verify() method.
+    // Decodes the JWT token to retrieve the payload generated during token creation using jwt.sign(). If the token is invalid, jwt.verify() throws an error.
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
 
+
+    // If no user is found with the decoded token's ID, it indicates an invalid access token.
     if (!user) {
         throw new ApiError(401, "Invalid Access Token")       
     }
 
     req.user = user;
+
     next();
 
 })
