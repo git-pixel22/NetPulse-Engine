5eefff37ad0587a5a6db2195879ef31874deb0f4
git-pixel22 <workemail.rajat@gmail.com>
2024-05-21 19:57:06 +0530

    adds toggleSubscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -7,8 +7,41 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const {channelId} = req.params;
+    const subscriberId = req.user._id;
     // TODO: toggle subscription
+    if(!channelId.trim() || !isValidObjectId(channelId)) {
+        throw new ApiError(400, "Channel Does Not Exist")
+    }
+
+
+    // Check if the channel is already subscribed
+
+    const subscribedChannel = await Subscription.findOne({
+        subscriber: subscriberId,
+        channel: channelId
+    })
+
+    let subscriberStatus;
+
+    console.log(subscribedChannel);
+
+    if(subscribedChannel) {
+        await Subscription.findByIdAndDelete(subscribedChannel._id);
+        subscriberStatus = "Unsubscribed";
+    } else {
+        await Subscription.create({
+            subscriber: subscriberId,
+            channel: channelId
+        });
+        subscriberStatus = "Subscribed";
+    }
+    
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscriberStatus, "Channel Subscribe Status Toggled Successfully")
+    )
 })
 
 // controller to return subscriber list of a channel
